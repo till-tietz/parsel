87fd206442dcdda231772e4097132ca8fefa0227
till-tietz <ttietz2014@gmail.com>
2022-04-14 15:35:27 +0200

    fixed piping functionality and updates constructors

--- R/constructors_navigation.R
+++ R/constructors_navigation.R
@@ -1,15 +1,19 @@
-#' wrapper around \code{RSelenium remDr$mavigate} to generate safe navigation code
+#' wrapper around remDr$mavigate to generate safe navigation code
 #'
-#'|@param url a character string specifying the name of the variable holding the url string or the url string the function should navigate to.
+#' @param url a character string specifying the name of the variable holding the url string or the url string the function should navigate to.
+#' @param prev a placeholder for the output of functions being piped into go(). Defaults to NULL and should not be altered.
 #' @return a character string defining 'RSelenium' navigation instructions that can be pasted into a scraping function
 #' @export
 #'
 #' @examples
+#' \dontrun{
 #'
 #' go("https://www.wikipedia.org/")
+#'
+#' }
 
 
-go <- function(url){
+go <- function(url, prev = NULL){
 
   if(!is.character(url)){
     stop("url is not of type character")
@@ -30,21 +34,27 @@ go <- function(url){
 
   out <- paste(not_loaded, navigate, while_loop, sep = "\n")
 
-
+  if(!is.null(prev)){
+    out <- paste(prev, out, sep = " \n \n ")
+  }
 
   return(out)
 }
 
-#' wrapper around \code{RSelenium remDr$goBack} to generate safe backwards navigation code
+#' wrapper around remDr$goBack to generate safe backwards navigation code
 #'
+#' @param prev a placeholder for the output of functions being piped into goback(). Defaults to NULL and should not be altered.
 #' @return a character string defining 'RSelenium' backwards navigation instructions that can be pasted into a scraping function
 #' @export
 #'
 #' @examples
+#' \dontrun{
 #'
 #' goback()
+#'
+#' }
 
-goback <- function(){
+goback <- function(prev = NULL){
 
   not_returned <- "not_returned <- TRUE"
 
@@ -63,13 +73,11 @@ goback <- function(){
 
   out <- paste(not_returned, from, go_back, while_loop, sep = "\n")
 
+  if(!is.null(prev)){
+    out <- paste(prev, out, sep = " \n \n ")
+  }
+
   return(out)
 }
-
-
-
-
-
-
 
 
